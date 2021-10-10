b561b9b9f8bebbf495230a04ec4cd846b2c76f90
Alexis <pinceminalexis@gmail.com>
2021-10-10 16:52:11 +0200

    ajout code Alexis

--- TP3/Code Alexis/couleur_compteur.c
+++ TP3/Code Alexis/couleur_compteur.c
@@ -9,7 +9,7 @@ Objectif : comptez le nombre de couleurs identique dans un tableau de 100 couleu
 #include <stdlib.h>
 #include <time.h>
 
-#define NOMBRE_COULEURS 100
+#define NOMBRE_COULEURS 5
 
 int main(){
 
@@ -22,6 +22,8 @@ typedef struct couleur{
 	unsigned char bleu;
 	unsigned char alpha;
 }couleur;
+
+
 
 srand(time(NULL));
 
@@ -42,40 +44,54 @@ for(int i = 0; i < NOMBRE_COULEURS; i++){
 }
 
 
-couleur tabCouleursCompte[NOMBRE_COULEURS];
+typedef struct couleurCompte{
+	unsigned char rouge;
+	unsigned char vert;
+	unsigned char bleu;
+	unsigned char alpha;
+	unsigned int compteur;
+}couleurCompte;
 
 
-/*boucle qui parcours les couleurs*/
-for(int i = 0; i < NOMBRE_COULEURS-1; i++){
-	/*compteur renitialisé à chaque couleur*/
-	int compteur = 1;
-	/*boucle for qui parcours les couleurs qui se trouve après la ième*/
-	for(int j = i+1; j < NOMBRE_COULEURS; j++){
-		/* on compare la couleur i avec les couleurs suivantes si elles sont égales alors les ajoutes dans le tableau des couleur comptés après avoir vérifié qu'elles n'y était pas déjà et on incrémente sinon on l'ajoute uniquement*/
-		if(tableauCouleurs[i].rouge == tableauCouleurs[j].rouge && tableauCouleurs[i].vert == tableauCouleurs[j].vert && tableauCouleurs[i].bleu == tableauCouleurs[j].bleu && tableauCouleurs[i].alpha == tableauCouleurs[j].alpha){
-			/*boucle for qui permet de parcourir le tableau des couleurs comptées*/
-			for(int h = 0; h < NOMBRE_COULEURS; h++){
-				/*on compare la couleur ième avec les couleurs déjà pésente dans le tableau des couleurs comptées si elle n'y est pas alors on l'ajoute sinon on incrémente le compteur*/
-				if(tabCouleursCompte[h].rouge != tableauCouleurs[i].rouge || tabCouleursCompte[h].vert != tableauCouleurs[i].vert || tabCouleursCompte[h].bleu != tableauCouleurs[i].bleu || tabCouleursCompte[h].alpha != tableauCouleurs[i].alpha){
-				tabCouleursCompte[i].rouge = tableauCouleurs[i].rouge;
-				tabCouleursCompte[i].vert = tableauCouleurs[i].vert;
-				tabCouleursCompte[i].bleu = tableauCouleurs[i].bleu;
-				tabCouleursCompte[i].alpha = tableauCouleurs[i].alpha;
-				}
-				else
-					++compteur;
-			}
-		}
-		else{
-			    tabCouleursCompte[i].rouge = tableauCouleurs[i].rouge;
-				tabCouleursCompte[i].vert = tableauCouleurs[i].vert;
-				tabCouleursCompte[i].bleu = tableauCouleurs[i].bleu;
-				tabCouleursCompte[i].alpha = tableauCouleurs[i].alpha;
+couleurCompte tabCouleursCompte[NOMBRE_COULEURS];
+
+for(int i=0; i<NOMBRE_COULEURS; i++){
+	tabCouleursCompte[i].rouge = 0;
+	tabCouleursCompte[i].vert = 0;
+	tabCouleursCompte[i].bleu = 0;
+	tabCouleursCompte[i].alpha = 0;
+	tabCouleursCompte[i].compteur = 0;
+}
+
+/*boucle qui parcours les couleurs les unes après les autres*/
+for(int i = 0; i < NOMBRE_COULEURS; i++){
+	/*boucle for qui parcours toutes les couleurs*/
+	for(int j = 0; j < NOMBRE_COULEURS; j++){
+		/* si la couleur n'est pas encore comptabilisé on l'ajoute et on initialise à 1 les compteurs*/
+		if(tabCouleursCompte[j].compteur == 0){
+			tabCouleursCompte[j].rouge = tableauCouleurs[i].rouge;
+			tabCouleursCompte[j].vert = tableauCouleurs[i].vert;
+			tabCouleursCompte[j].bleu = tableauCouleurs[i].bleu;
+			tabCouleursCompte[j].alpha = tableauCouleurs[i].alpha;
+			tabCouleursCompte[j].compteur = 1;
+			break;
+		/*si on a déjà rencontré la valeur alors on la compart avec la valeur actuelle qui se trouve en i*/
+		}else if(tableauCouleurs[i].rouge == tableauCouleurs[j].rouge && tableauCouleurs[i].vert == tableauCouleurs[j].vert && tableauCouleurs[i].bleu == tableauCouleurs[j].bleu && tableauCouleurs[i].alpha == tableauCouleurs[j].alpha){
+			++tabCouleursCompte[j].compteur;
+			break;
+
 		}
 	}
-	/*à chaque itération de la boucle for (i) on affiche les couleurs ajouter dans le tableau des couleurs comptées*/
-	printf("couleur %d : rouge = 0x%x, vert = 0x%x, bleu = 0x%x, alpha = 0x%x %d\n",i+1,tabCouleursCompte[i].rouge,tabCouleursCompte[i].vert,
-	tabCouleursCompte[i].bleu,tabCouleursCompte[i].alpha, compteur);
+}
+
+/*on affiche uniquement les couleurs qui ont été ajouté dans le tableau != 0*/
+int numeroCouleur = 0;
+for(int i = 0; i < NOMBRE_COULEURS; i++){
+	if((tabCouleursCompte[i].rouge && tabCouleursCompte[i].vert && tabCouleursCompte[i].bleu && tabCouleursCompte[i].alpha) != 0){
+	++numeroCouleur;
+	printf("couleur %d : rouge = 0x%x, vert = 0x%x, bleu = 0x%x, alpha = 0x%x %d\n",numeroCouleur,tabCouleursCompte[i].rouge,tabCouleursCompte[i].vert,
+	tabCouleursCompte[i].bleu,tabCouleursCompte[i].alpha,tabCouleursCompte[i].compteur);
+	}
 }
 return 0;
 }
